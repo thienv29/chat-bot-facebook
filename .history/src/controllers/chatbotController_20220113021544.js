95a3bb825997b13d3a887ff1dfc0763cf7560e11
thienv29 <thienv29@gmail.com>
2022-01-13 02:15:55 +0700

    update

--- .history/src/controllers/chatbotController_20220113021544.js
+++ .history/src/controllers/chatbotController_20220113021544.js
@@ -84,7 +84,7 @@ function callSendAPI(sender_psid, response) {
         },
         "message": response
     }
-    console.log(response)
+    console.log()
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
